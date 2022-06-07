6d121a6943e5dd65c0562932d486733077e03cb8
Luis Felipe Hernandez Mora <luisfelipehernandezmora@gmail.com>
2022-06-07 16:44:33 +0530

    added usern-name and password

--- website_test.py
+++ website_test.py
@@ -13,14 +13,29 @@ options.add_argument('--ignore-certificate-errors')
 options.add_argument('--test-type')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get(url)
-time.sleep(2)
-driver.close()
-
+# time.sleep(2.5)
 
 # assert "Swag Labs" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.clear()
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-# driver.close()
+
+selected_user=input(f"""\nWhich user you will like to use? You can choose between\n
+    standard_user (it wont give any problem :) ),
+    locked_out_user (is banned from the website). 
+    problem_user (will suffer a lot of problems), 
+    performance_glitch_user (will be a little stuck while browsing) \n\n""")
+
+## Insert User name
+user_name = driver.find_element_by_id("user-name")
+user_name.clear() #delete anything previously typed
+user_name.send_keys(selected_user)
+
+time.sleep(2.5)
+
+## Insert password
+# secret_password=input(f"What is the password? ")
+secret_password='secret_sauce'  
+password = driver.find_element_by_id("password")
+password.clear() #delete anything previously typed
+password.send_keys(secret_password)
+time.sleep(2.5)
+
+driver.close()
